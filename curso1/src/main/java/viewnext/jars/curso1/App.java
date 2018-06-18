d8730e2a2718f5b087d4a8a09c822474e715f90c
fballarin <fjballarin@viewnext.com>
2018-06-18 17:32:41 +0200

    new version

---
+++ curso1/src/main/java/viewnext/jars/curso1/App.java
@@ -0,0 +1,13 @@
+package viewnext.jars.curso1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
